ecc2d68ca394c16616e693f1a76ef4f8f1af981a
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2026-05-20 15:35:38 +0300

    init

---
+++ src/test/java/com/matvey/innowiseuserservice/InnowiseUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matvey.innowiseuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
